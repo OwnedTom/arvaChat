dfeb18be7db82a4922183150db46f9ffeaf3fb0a
OwnedTom <tom.steele.94@gmail.com>
2017-03-06 12:34:06 +0100

    Hide sensitive data

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ export class App extends ArvaApp {
     static defaultDataSource() {
         /* Firebase initialization */
         firebase.initializeApp({
-            apiKey: "AIzaSyCqKyW2EDtInPEaLE9p0MkMULewbcJVjVA",
-            authDomain: "bizboard-test.firebaseapp.com",
-            databaseURL: "https://bizboard-test.firebaseio.com",
-            storageBucket: "bizboard-test.appspot.com",
+            apiKey: '<api-key>',
+            authDomain: '<subdomain>.firebaseapp.com',
+            databaseURL: 'https://<subdomain>.firebaseio.com',
+            storageBucket: '<subdomain>.appspot.com'
             messagingSenderId: "339656294433"
         });
         return new FirebaseDataSource('/', {});
